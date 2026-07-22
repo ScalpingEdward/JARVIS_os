f0e9054d5892d8f2fd984cfead5c9c26c1e259ff
Phoenix - Life Solutions (GMB LABs) <branislav.gombos@gmail.com>
2026-07-22 14:16:12 +0200

    Add PHOENIX v21.20 module exports

---
+++ backend/app/modules/self_learning_performance_optimizer/__init__.py
@@ -0,0 +1,6 @@
+"""PHOENIX v21.20 self-learning performance optimizer."""
+
+from .router import router
+from .service import SelfLearningPerformanceOptimizerService
+
+__all__ = ["router", "SelfLearningPerformanceOptimizerService"]
